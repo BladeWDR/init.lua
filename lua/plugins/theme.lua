ca1cc089d5ee1dfb5073c09154c05af49a264027
BladeWDR <ssbarning@gmail.com>
2024-01-05 18:25:22 -0500

    switched to rose-pine theme + some cleanup work

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,28 @@
+return {
+
+    --start colorscheme when loading Neovim.
+    {
+        "rose-pine/neovim",
+        lazy = false, -- ensure that this is loaded at startup.
+        priority = 1000,
+        config = function()
+            -- actually load the colorscheme.
+            vim.cmd([[colorscheme rose-pine]])
+        end,
+
+    }
+
+}
+-- return {
+--     -- start colorscheme when loading Neovim.
+--     {
+--         "navarasu/onedark.nvim",
+--         lazy = false, -- ensure that this is loaded at startup.
+--         priority = 1000,
+--         config = function()
+--             -- actually load the colorscheme.
+--             vim.cmd([[colorscheme onedark]])
+--         end,
+--     }
+--
+-- }
