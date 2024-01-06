5d8072f877ed470410c25f1378d4313ae9bd5c36
BladeWDR <ssbarning@gmail.com>
2024-01-06 01:30:13 -0500

    make background transparent

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,9 +6,15 @@ return {
         lazy = false, -- ensure that this is loaded at startup.
         priority = 1000,
         name = "rose-pine",
+        opts = {
+            disable_background = true,
+            disable_float_background = true,
+        },
         config = function()
-            -- actually load the colorscheme.
-            vim.cmd([[colorscheme rose-pine]])
+            -- actually load the colorscheme
+            vim.cmd.colorscheme "rose-pine"
+            vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+            vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
         end,
 
     }
