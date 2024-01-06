bf2e74f87731040f616eaa52fa984043594dec16
Scott Barning <ssbarning@gmail.com>
2024-01-05 21:27:19 -0500

    added treesitter parser for powershell

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "ansiblels","bashls","docker_compose_language_service","pyright","terraformls","hydra_lsp"}
+                ensure_installed = {"lua_ls", "ansiblels","bashls","docker_compose_language_service","pyright","terraformls","hydra_lsp","powershell_es"}
             })
         end
     },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -44,6 +44,15 @@ return {
         auto_install = true,
         additional_vim_regex_highlighting = false,
       })
+            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+            parser_config.powershell = {
+                install_info = {
+                    url = "https://github.com/jrsconfitto/tree-sitter-powershell",
+                    files = {"src/parser.c"}
+                },
+                filetype = "ps1",
+                used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" }
+            }
     end,
   },
 }
