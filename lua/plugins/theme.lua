4eeb78cf93fb2134dc7d11d2ccdba626433943d3
Scott Barning <ssbarning@gmail.com>
2024-01-05 23:27:46 -0500

    remove old commented out theme.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -14,16 +14,3 @@ return {
     }
 
 }
--- return {
---     -- start colorscheme when loading Neovim.
---     {
---         "navarasu/onedark.nvim",
---         lazy = false, -- ensure that this is loaded at startup.
---         priority = 1000,
---         config = function()
---             -- actually load the colorscheme.
---             vim.cmd([[colorscheme onedark]])
---         end,
---     }
---
--- }
