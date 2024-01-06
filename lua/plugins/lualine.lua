75474b51f0158aef252a3655141ac2145d05439d
Scott Barning <ssbarning@gmail.com>
2024-01-06 01:09:29 -0500

    switched lualine theme again, added nvim-base16

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,24 @@
   return    {
         {
+    "RRethy/nvim-base16",
+          lazy = false,
+          priority=10000,
+          config = function()
+              vim.cmd('colorscheme base16-rose-pine')
+          end
+      },
+        {
             "nvim-lualine/lualine.nvim",
-            dependencies = { "nvim-tree/nvim-web-devicons" },
+          dependencies = {
+              "nvim-tree/nvim-web-devicons",
+              "RRethy/nvim-base16"},
             lazy = false,
             priority = 9000,
             config = function()
             local lualine = require("lualine")
             lualine.setup({
               options = {
-                theme = "nightfly",
+                theme = "base16",
               },
             })
             end,
