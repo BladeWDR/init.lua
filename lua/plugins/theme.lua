7859605791e56e83763869b4e0d0371509c06a3f
Scott Barning <ssbarning@gmail.com>
2024-01-05 20:17:41 -0500

    add custom name for rose-pine plugin spec.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,7 @@ return {
         "rose-pine/neovim",
         lazy = false, -- ensure that this is loaded at startup.
         priority = 1000,
+        name = "rose-pine",
         config = function()
             -- actually load the colorscheme.
             vim.cmd([[colorscheme rose-pine]])
