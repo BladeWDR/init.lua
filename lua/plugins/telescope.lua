145f07ae0a594a952f9adad71a23d352e60a8bb8
BladeWDR <ssbarning@gmail.com>
2024-01-09 22:22:58 -0500

    added additional keymaps for telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,9 +11,16 @@ return {
             -- keymaps here
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
             vim.keymap.set('n', 'c-p', builtin.git_files, {})
+            vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = '[G]oto [D]efinition'})
+            vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = '[G]oto [R]eferences'})
+            vim.keymap.set('n', 'gI', builtin.lsp_implementations, { desc = '[G]oto [I]mplementation'})
+            vim.keymap.set('n', '<leader>D', builtin.lsp_type_definitions, { desc = 'Type [D]efinition'})
+            vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = '[D]ocument [S]ymbols'})
+            vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols'})
         end
     },
     {
