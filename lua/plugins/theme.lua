1dc23e36758d488d1a915d988925fd9ad203ce0d
BladeWDR <ssbarning@gmail.com>
2024-03-30 00:11:23 -0400

    updated rose-pine theme for breaking changes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,16 +7,17 @@ return {
         priority = 1000,
         name = "rose-pine",
         opts = {
-            disable_background = true,
-            disable_float_background = true,
+            enable = {
+                migrations = true,
+            },
+            styles = {
+                transparency = false,
+            },
+            highlight_groups = {
+                Normal = { bg = "none" },
+                NormalFloat = { bg = "none" },
+            }
         },
-        config = function()
-            -- actually load the colorscheme
-            vim.cmd.colorscheme "rose-pine"
-            vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-            vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-        end,
-
     }
 
 }
