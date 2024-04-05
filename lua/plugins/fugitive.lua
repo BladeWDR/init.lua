031e7ebe591adc4a53bc7b291d179f16d547c9ed
BladeWDR <ssbarning@gmail.com>
2024-04-04 23:41:16 -0400

    update keymaps for fugitive.

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,14 @@
 return {
     "tpope/vim-fugitive",
-    cmd = "Git"
+    cmd = "Git",
+
+    -- Keymaps
+    vim.keymap.set('n', '<leader>gs', ":G status<CR>"),
+    vim.keymap.set('n', '<leader>ga', ":G add %<CR>"),
+    vim.keymap.set('n', '<leader>fg', ":G fetch<cr>"),
+    vim.keymap.set('n', '<leader>gd', ":G diff<cr>"),
+    vim.keymap.set('n', '<leader>bg', ":G blame<cr>"),
+    vim.keymap.set('n', '<leader>gc', ":G commit<cr>"),
+    vim.keymap.set('n', '<leader>gu', ":G push<cr>"),
+
 }
