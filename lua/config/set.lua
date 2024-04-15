260293a2b46ef24cef65446187f1cbcd4b20e467
Scott Barning <ssbarning@gmail.com>
2024-04-14 20:20:24 -0400

    stole highlight autocmd from kickstart

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -44,3 +44,10 @@ vim.opt.updatetime = 50
 
 vim.opt.fileformat = unix
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
