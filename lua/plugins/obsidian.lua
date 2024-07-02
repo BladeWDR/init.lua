bdc43049495131fecc9e8efec6db0ff2fcce6852
BladeWDR <scott@ssb-tech.net>
2024-07-02 15:32:38 -0400

    forgot space

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,7 +6,7 @@ return {
     -- vim.fn.expand needed here to expand "~".
     -- These events make it so that the obsidian.nvim plugin is only loaded in the vault. 
     "BufReadPre " .. vim.fn.expand "~" .. "/git/wiki/**.md",
-    "BufNewFile" .. vim.fn.expand "~" .. "/git/wiki/**.md",
+    "BufNewFile " .. vim.fn.expand "~" .. "/git/wiki/**.md",
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
