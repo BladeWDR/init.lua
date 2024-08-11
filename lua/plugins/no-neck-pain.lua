86f2e7f6716a4139286327d861f05ef53b54135f
BladeWDR <scott@ssb-tech.net>
2024-08-11 18:40:28 -0400

    add no-neck-pain plugin, add ftplugin for markdown.

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,11 @@
+return {
+    "shortcuts/no-neck-pain.nvim",
+    ft = { "markdown" },
+    version = "*",
+    cmd = "NoNeckPain",
+    keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
+    opts = {
+      width = 100,
+    },
+}
+
