d60224deb4ce080e49ed036b071a44b144bf2c1f
BladeWDR <scott@ssb-tech.net>
2024-10-11 18:06:54 -0400

    enable hidden files in telescope find_files picker.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,6 +31,12 @@ return {
                         require("telescope.themes").get_dropdown {
                         }
                     }
+                },
+                pickers = {
+                    find_files = {
+                        -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                    }
                 }
             })
             require("telescope").load_extension("ui-select")
