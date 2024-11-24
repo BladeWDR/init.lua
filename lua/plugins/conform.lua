369ffc11eb2d829b8b55023b6201c7de4e425dab
BladeWDR <scott@ssb-tech.net>
2024-11-23 20:32:03 -0500

    add Obsidian friendly Markdown formatter.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,7 @@ return { -- Autoformat
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+	enabled = false,
 	keys = {
 		{
 			"<leader>f",
@@ -18,7 +19,7 @@ return { -- Autoformat
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true, md = true }
+			local disable_filetypes = { c = true, cpp = true }
 			local lsp_format_opt
 			if disable_filetypes[vim.bo[bufnr].filetype] then
 				lsp_format_opt = "never"
@@ -32,6 +33,7 @@ return { -- Autoformat
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
+			markdown = { "markdown-oxide" },
 			-- Conform can also run multiple formatters sequentially
 			-- python = { "isort", "black" },
 			--
