231fd361b92d2c3b1bcbfea18bfe469f72e31345
BladeWDR <1136623+BladeWDR@users.noreply.github.com>
2025-03-31 19:21:27 -0400

    switched from nvim-cmp to blink.cmp

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,123 @@
+return {
+	'saghen/blink.cmp',
+	-- optional: provides snippets for the snippet source
+	dependencies = { 
+		'rafamadriz/friendly-snippets', 
+		'ribru17/blink-cmp-spell',
+		'L3MON4D3/LuaSnip', version = 'v2.*',
+		build = (function()
+			-- Build Step is needed for regex support in snippets.
+			-- This step is not supported in many windows environments.
+			-- Remove the below condition to re-enable on windows.
+			if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+				return
+			end
+			return "make install_jsregexp"
+		end)(),
+	},
+
+	-- use a release tag to download pre-built binaries
+	version = '1.0.0',
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		snippets = { preset = 'luasnip' },
+
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		-- 'super-tab' for mappings similar to vscode (tab to accept)
+		-- 'enter' for enter to accept
+		-- 'none' for no mappings
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		--
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = 'default',
+			['<C-p>'] = { 'select_prev', 'fallback' },
+			['<C-n>'] = { 'select_next', 'fallback' },
+
+			-- show with a list of providers
+			['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
+
+
+		},
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = 'mono'
+		},
+
+		completion = { 
+			documentation = { auto_show = false },
+			menu = { 
+				draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } } }
+			}
+		},
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+		default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'spell' },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
+				spell = {
+					name = 'Spell',
+					module = 'blink-cmp-spell',
+					opts = {
+						-- EXAMPLE: Only enable source in `@spell` captures, and disable it
+						-- in `@nospell` captures.
+						enable_in_context = function()
+							local curpos = vim.api.nvim_win_get_cursor(0)
+							local captures = vim.treesitter.get_captures_at_pos(
+								0,
+								curpos[1] - 1,
+								curpos[2] - 1
+							)
+							local in_spell_capture = false
+							for _, cap in ipairs(captures) do
+								if cap.capture == 'spell' then
+									in_spell_capture = true
+								elseif cap.capture == 'nospell' then
+									return false
+								end
+							end
+							return in_spell_capture
+						end,
+					},
+				},
+
+			},
+		},
+
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = {
+			sorts = {
+				function(a, b)
+					local sort = require('blink.cmp.fuzzy.sort')
+					if a.source_id == 'spell' and b.source_id == 'spell' then
+						return sort.label(a, b)
+					end
+				end,
+				-- This is the normal default order, which we fall back to
+				'score',
+				'kind',
+				'label',
+			},
+		},
+
+	},
+	opts_extend = { "sources.default" }
+}
