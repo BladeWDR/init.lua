625fdd643ea410fd2c7b06ea404d614efd5fb170
BladeWDR <1136623+BladeWDR@users.noreply.github.com>
2025-04-03 09:48:29 -0400

    removing coc.nvim and extra nvim-lspconfig declaration
    fixes the issues I was having with LSP clients not attaching.

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,5 +1,4 @@
 return {
-    {
         "kevinhwang91/nvim-ufo",
         dependencies = { "kevinhwang91/promise-async" },
         config = function()
@@ -13,26 +12,5 @@ return {
                 end
             })
         end,
-    },
-    {
-        "neoclide/coc.nvim",
-        branch = "master",
-        build = "yarn install --frozen-lockfile",
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.foldingRange = {
-                dynamicRegistration = false,
-                lineFoldingOnly = true,
-            }
-            
-            local lspconfig = require("lspconfig")
-            for _, ls in ipairs(vim.lsp.get_clients()) do
-                lspconfig[ls].setup({ capabilities = capabilities })
-            end
-        end,
-    },
 }
 
