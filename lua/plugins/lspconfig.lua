5d3572e14830a0f15a0ec82c7e23ba77b180a3b1
BladeWDR <1136623+BladeWDR@users.noreply.github.com>
2025-04-02 23:01:13 -0400

    Revert "switched from nvim-cmp to blink.cmp"
    This reverts commit 231fd361b92d2c3b1bcbfea18bfe469f72e31345.
    
    too many weird issues and things that don't support blink yet.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -21,14 +21,13 @@ return {
 			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-      "saghen/blink.cmp",
 
 			-- Useful status updates for LSP.
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
 
 			-- Allows extra capabilities provided by nvim-cmp
-			-- "hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			-- Brief aside: **What is LSP?**
@@ -170,7 +169,7 @@ return {
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").default_capabilities())
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 			-- Enable the following language servers
 			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
