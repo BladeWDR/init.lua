3a2a90a0f45193ff0b36a1d63a2060aa8f32cd0f
BladeWDR <1136623+BladeWDR@users.noreply.github.com>
2025-05-21 22:10:37 -0400

    remove luasnip, just use friendly-snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,38 +4,13 @@ return {
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 		"ribru17/blink-cmp-spell",
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		build = (function()
-			-- Build Step is needed for regex support in snippets.
-			-- This step is not supported in many windows environments.
-			-- Remove the below condition to re-enable on windows.
-			if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-				return
-			end
-			return "make install_jsregexp"
-		end)(),
 	},
 
 	-- use a release tag to download pre-built binaries
-	version = "1.0.0",
+	version = "1.*",
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		snippets = { preset = "luasnip" },
-
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = {
 			preset = "default",
 			["<C-p>"] = { "select_prev", "fallback" },
